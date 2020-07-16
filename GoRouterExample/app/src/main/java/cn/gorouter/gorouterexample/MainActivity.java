a8b3611ee3d27e06640e4548c86ef2f761d79b25
Rubintry <13857769302@163.com>
2020-07-16 14:04:42 +0800

    Add an example.

--- GoRouterExample/app/src/main/java/cn/gorouter/gorouterexample/MainActivity.java
+++ GoRouterExample/app/src/main/java/cn/gorouter/gorouterexample/MainActivity.java
@@ -1,11 +1,15 @@
 package cn.gorouter.gorouterexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import cn.gorouter.annotation.Route;
 import cn.gorouter.api.launcher.GoRouter;
+import cn.gorouter.api.logger.GoLogger;
 
 /**
  * @author logcat
@@ -19,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        GoRouter.getInstance().build("/another/AnotherActivity").go();
+        GoRouter.getInstance()
+                .build("/another/AnotherActivity")
+                .go(this , 1001);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 1001:
+                GoLogger.info("Back from another Activity!!!");
+                break;
+            default:
+                break;
+        }
     }
 }
